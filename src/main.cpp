bd7a93cf52307d0c78d32ce452714802fa0e8b25
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-01-01 17:50:38 -0600

    window created

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,47 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+void init(GLFWwindow* window) {};
+
+void display(GLFWwindow* window, double currentTime)
+{
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 int main()
 {
-    std::cout << "Hello There!" << "\n";
-    return 0;
+
+    // init glfw
+    if(!glfwInit()) {exit(EXIT_FAILURE);}
+
+    //opengl contexts
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    //make window
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    //init glew
+    if(glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
+
+    //turn of v-sync
+    glfwSwapInterval(1);
+
+    init(window);
+
+    // handle exit code
+    while(!glfwWindowShouldClose(window)) 
+    {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // termination code
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
 }
