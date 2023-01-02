bb94274cde2e99d14f6b4aba693c21e24856fcba
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-01-01 21:03:36 -0600

    vertex and fragment shaders

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,54 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-void init(GLFWwindow* window) {};
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() 
+{
+    const char* vShaderSource =
+        "#version 430 \n"
+        "void main(void) \n"
+        "{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}";
+
+    const char* fShaderSource = 
+        "#version 430 \n"
+        "out vec4 color; \n"
+        "void main(void) \n"
+        "{color = vec4(0.0, 0.0, 1.0, 1.0);}";
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vShaderSource, NULL);
+    glShaderSource(fShader, 1, &fShaderSource, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
+
+}
+
+void init(GLFWwindow* window) 
+{
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+
+};
 
 void display(GLFWwindow* window, double currentTime)
 {
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    glPointSize(30.0f);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main()
