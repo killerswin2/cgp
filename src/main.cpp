7636853e02fd93ead89c942fe7a5544e2cf71b7c
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-01-04 17:32:00 -0600

    added support for debugMessages

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,156 @@
 #define numVAOs 1
 
 GLuint renderingProgram;
-GLuint vao[numVAOs];
+GLuint vao[numVAOs]; 
+
+
+void APIENTRY debugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void *data)
+{
+    char* _source;
+    char* _type;
+    char* _severity;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+        _source = "API";
+        break;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        _source = "WINDOW SYSTEM";
+        break;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        _source = "SHADER COMPILER";
+        break;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+        _source = "THIRD PARTY";
+        break;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+        _source = "APPLICATION";
+        break;
+
+        case GL_DEBUG_SOURCE_OTHER:
+        _source = "UNKNOWN";
+        break;
+
+        default:
+        _source = "UNKNOWN";
+        break;
+    }
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+        _type = "ERROR";
+        break;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        _type = "DEPRECATED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        _type = "UDEFINED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+        _type = "PORTABILITY";
+        break;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+        _type = "PERFORMANCE";
+        break;
+
+        case GL_DEBUG_TYPE_OTHER:
+        _type = "OTHER";
+        break;
+
+        case GL_DEBUG_TYPE_MARKER:
+        _type = "MARKER";
+        break;
+
+        default:
+        _type = "UNKNOWN";
+        break;
+    }
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+        _severity = "HIGH";
+        break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+        _severity = "MEDIUM";
+        break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+        _severity = "LOW";
+        break;
+
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+        _severity = "NOTIFICATION";
+        break;
+
+        default:
+        _severity = "UNKNOWN";
+        break;
+    }
+
+    printf("%d: %s of %s severity, raised from %s: %s\n",
+            id, _type, _severity, _source, msg);
+}
+
+
+void printShaderLog(GLuint shader)
+{
+    int length = 0;
+    int chWrittenToLog = 0;
+    char *log;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+    if (length > 0 ) 
+    {
+        log = new char[length];
+        glGetShaderInfoLog(shader, length, &chWrittenToLog, log);
+        std::cerr << "Shader Info log: " << log << "\n";
+        delete log;
+    }
+}
+
+void printProgramLog(GLuint program) 
+{
+    int length = 0;
+    int chWrittenToLog = 0;
+    char *log;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
+    if(length > 0)
+    {
+        log = new char[length];
+        glGetShaderInfoLog(program, length, &chWrittenToLog, log);
+        std::cerr << "Program Info log: " << log << "\n";
+        delete log;
+    }
+
+}
+
+bool checkOpenGLError()
+{
+    bool foundError = false;
+    int glError = glGetError();
+    while(glError != GL_NO_ERROR)
+    {
+        std::cerr << "glError: " << glError << "\n";
+        foundError = true;
+        glError = glGetError();
+    }
+    return glError;
+}
 
 GLuint createShaderProgram() 
 {
+    GLuint vShaderCompiled;
+    GLuint fShaderCompiled;
+    GLuint shaderLinked;
+
     const char* vShaderSource =
         "#version 430 \n"
         "void main(void) \n"
@@ -17,11 +163,13 @@ GLuint createShaderProgram()
     const char* fShaderSource = 
         "#version 430 \n"
         "out vec4 color; \n"
-        "void main(void) \n"
-        "{color = vec4(0.0, 0.0, 1.0, 1.0);}";
+        "void main(void) \n"        // we can access the coordinates of the fragment with gl_FragCoord
+        "{if(gl_FragCoord.x < 295) {color = vec4(1.0, 0.0, 0.0, 1.0);} else {color = vec4(0.0, 0.0, 1.0, 1.0);}}";
 
     GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    checkOpenGLError();
     GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    checkOpenGLError();
 
     glShaderSource(vShader, 1, &vShaderSource, NULL);
     glShaderSource(fShader, 1, &fShaderSource, NULL);
@@ -68,6 +216,11 @@ int main()
 
     //init glew
     if(glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
+
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(debugMessageCallback, nullptr);
 
     //turn of v-sync
     glfwSwapInterval(1);
