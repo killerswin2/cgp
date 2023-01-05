f8100e756492a9e7c4274414b957ed3ca014475b
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-01-04 23:22:03 -0600

    shaders and c++17

---
+++ complie.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.236.0\Bin\glslc.exe -fshader-stage=fragment  shaders\fragmentShaderSimple.glsl -o shaders\fragmentShaderSimple.spv 
+C:\VulkanSDK\1.3.236.0\Bin\glslc.exe -fshader-stage=vertex  shaders\vertexShaderSimple.glsl -o shaders\vertexShaderSimple.spv 
+pause
---
+++ shaders/fragmentShaderSimple.glsl
@@ -0,0 +1,12 @@
+#version 430
+
+out vec4 color;
+void main(void)      // we can access the coordinates of the fragment with gl_FragCoord
+{
+    if(gl_FragCoord.x < 295) 
+    {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    } else {
+        color = vec4(0.0, 0.0, 1.0, 1.0);
+    }
+}
